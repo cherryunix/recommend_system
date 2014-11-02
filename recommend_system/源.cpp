8cdcab25b9718cc68ce3090ae0d536bd0c1fafbb
cherryunix <hzwqwe@gmail.com>
2014-11-02 16:49:00 +0800

    First Time

---
+++ recommend_system/源.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class entity{
+private:
+	int attribute;
+	string name;
+public:
+	void add_entity(){
+
+	}
+};
+
+class relation{
+private:
+	int attribute;
+	 
+};
+
+class Graph{
+
+};
+
+int main()
+{
+
+}
